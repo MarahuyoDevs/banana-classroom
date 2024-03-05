9ff3940a2b220119b94d1b335f7656ad47382564
Karl Robeck Alferez <karlalferezfx@gmail.com>
2024-03-05 23:14:01 +0800

    add frontend test cases template

--- tests/service_v2/conftest.py
+++ tests/service_v2/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from banana_classroom.service_v2.app import api_service
+from banana_classroom.frontend.app import frontend_app
 from starlette.testclient import TestClient
 
 
 @pytest.fixture
-def service_v2_client() -> TestClient:
+def service_v2_backend_client() -> TestClient:
     return TestClient(api_service)
+
+
+@pytest.fixture
+def service_v2_frontend_client() -> TestClient:
+    return TestClient(frontend_app)
