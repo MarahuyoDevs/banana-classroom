dda71fe54afaa714d56d120e7daf669c6853e5e7
Karl Robeck Alferez <karlalferezfx@gmail.com>
2024-03-06 20:27:23 +0800

    add routing to signin and signup pages

--- banana_classroom/frontend/routes/signin/page.py
+++ banana_classroom/frontend/routes/signin/page.py
@@ -1,9 +1,12 @@
 from pypox.processing.base import processor
-from starlette.responses import PlainTextResponse
+from starlette.responses import PlainTextResponse, RedirectResponse
+
 from starlette.requests import Request
 from banana_classroom.frontend.app import template
 
 
 @processor()
 async def page(request: Request):
+    if "user_type" not in request.query_params:
+        return RedirectResponse("/signin?user_type=student")
     return template.TemplateResponse(request, "signin.html")
